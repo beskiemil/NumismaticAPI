e6850231e197fe1e217966b6066a6ae2afb3b944
Emil Beski <beskiemil@gmail.com>
2024-01-06 13:23:11 +0100

    refactor

--- src/api/type/services/type.js
+++ src/api/type/services/type.js
@@ -11,43 +11,21 @@ module.exports = createCoreService('api::type.type', ({ strapi }) => ({
     const params = this.getFetchParams(ctx);
 
     //jeżeli jest q to szukaj po q
-    const { q, ...restParams } = params;
+    const { q } = params;
     if (q) {
-      const { results, pagination } = await strapi
-        .service('api::type.type')
-        .findByQ(ctx);
-      return { results, pagination };
-    }
-
-    // jeżeli nie ma q to zwróć wszystkie
-    const types = await strapi.entityService.findMany('api::type.type', {
-      ...restParams,
-    });
-
-    //handle pagination
-    const page = parseInt(params?.pagination?.page) || 1;
-    const pageSize = parseInt(params?.pagination?.pageSize) || 25;
-    const { results, pagination } = handlePagination(types, page, pageSize);
-
-    return { results, pagination };
-  },
-
-  async findByQ(ctx) {
-    const params = this.getFetchParams(ctx);
-
-    //jeżeli jest q to szukaj po q
-    const { q, ...restParams } = params;
-    const types = await strapi.entityService.findMany('api::type.type', {
-      ...restParams,
-      filters: {
+      params.filters = {
         $or: [
           { title: { $containsi: q } },
           { series: { $containsi: q } },
           { commemorated_topic: { $containsi: q } },
         ],
-      },
-    });
+      };
+      delete params.q;
+    }
 
+    const types = await strapi.entityService.findMany('api::type.type', params);
+
+    //handle pagination
     const page = parseInt(params?.pagination?.page) || 1;
     const pageSize = parseInt(params?.pagination?.pageSize) || 25;
     const { results, pagination } = handlePagination(types, page, pageSize);
